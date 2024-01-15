5311e4f78256c47118fb6a8397d54e0b409cc8a0
Abdur Rahim <abdurrahim300431@gmail.com>
2024-01-16 02:52:58 +0600

    page update

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // sidebarNabMenu
+    let navigation = document.querySelector('.navigation');
+        let toggle = document.querySelector('.toggle');
+        toggle.onclick = function() {
+        navigation.classList.toggle('active');
+    }
+})

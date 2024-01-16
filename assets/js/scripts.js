18fcd2bcfc8554d88a3a4edcfd3c6540da3d6e4a
Sadbin Shakil <sadbin7979@gmail.com>
2024-01-16 23:28:10 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,3 +20,28 @@ $(document).ready(function(){
         });
     });
 })
+
+
+
+var count = 0; 
+  $(document).on('click','.sidebar_arrow',function(){
+    if(count == 0){ 
+        $(this).css({'transform':'rotate(180deg)'});
+        $('.sk_main_drp').hide(); 
+        $('.ar_navigation_logo a img').css({"width":"140px"}); 
+        $('.ar_dashboard_main_content_area').css({"flex":"0 0 95%"});    
+        $('.ar_dashboard_main_sidebar_area').css({"flex":"0 0 5%"});    
+      
+        count = 1;
+    }else{  
+
+        $(this).css({'transform':'rotate(0)'});
+        $('.sk_main_drp').show(); 
+        $('.ar_navigation_logo a img').css({"width":"250px"}); 
+        $('.ar_dashboard_main_content_area').css({"flex":"0 0 85%"});    
+        $('.ar_dashboard_main_sidebar_area').css({"flex":"0 0 15%"});  
+      
+        count = 0; 
+    }
+    return false;
+  }); 
